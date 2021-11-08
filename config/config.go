f8e3816a7280b3b58a2da36133701d7e9ac03175
Dan <orbz@users.noreply.github.com>
2021-11-08 13:24:25 -0500

    Adding in comments

--- config/config.go
+++ config/config.go
@@ -1,3 +1,4 @@
+// Package config adds additional functionality to the std flag package to allow overriding with environment variables.
 package config
 
 import (
@@ -26,10 +27,17 @@ func override(fs *flag.FlagSet, name string) error {
 	return nil
 }
 
+// Parse parses the command-line flags from os.Args[1:]. Must be called
+// after all flags are defined, before calls to package flag Parse functions
+//  and before flags are accessed by the program.
 func Parse() error {
 	return ParseFlagSet(os.Args[1:], flag.CommandLine)
 }
 
+// Parse parses flag definitions from the argument list, which should not include the command name.
+// Must be called after all flags in the FlagSet are defined, before calls to package flag Parse
+// functions  and before flags are accessed by the program.
+// The return value will be ErrHelp if -help or -h were set but not defined.
 func ParseFlagSet(args []string, fs *flag.FlagSet) error {
 	if fs.Parsed() {
 		return errors.New("config.Parse can only be called once and before flag package Parse")
